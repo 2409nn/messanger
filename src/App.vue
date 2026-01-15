770cf227a3104c2490e00fd3332562e01af6766b
Iskander07 <A.iskander07@gmail.com>
2026-01-16 01:33:18 +0600

    Создана общая структура проекта

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import sideMenu from "./components/sideMenu.vue"
+</script>
+
+<template>
+  <div>
+    <side-menu></side-menu>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
